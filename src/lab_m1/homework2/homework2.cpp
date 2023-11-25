caf51d0133a64fb06eea064f29c234777763b6bc
Ionut Gaina <gainaionutz@gmail.com>
2023-11-25 03:07:44 +0200

    create the tank and the position of the start camera

--- src/lab_m1/homework2/homework2.cpp
+++ src/lab_m1/homework2/homework2.cpp
@@ -5,63 +5,71 @@
 #include <iostream>
 
 using namespace std;
-using namespace m1;
-
+using namespace hw2;
 
 /*
  *  To find out more about `FrameStart`, `Update`, `FrameEnd`
  *  and the order in which they are called, see `world.cpp`.
  */
 
-
 Homework2::Homework2()
 {
 }
-
 
 Homework2::~Homework2()
 {
 }
 
-
 void Homework2::Init()
 {
+    renderCameraTarget = false;
+
+    fov = 50.f;
+
+    camera = new Camera();
+    camera->Set(glm::vec3(0, 7.5f, 10.0f), glm::vec3(0, 1, 0), glm::vec3(0, 1, 0));
+
     {
-        Mesh* mesh = new Mesh("box");
-        mesh->LoadMesh(PATH_JOIN(window->props.selfDir, RESOURCE_PATH::MODELS, "primitives"), "box.obj");
+        Mesh *mesh = new Mesh("plane");
+        mesh->LoadMesh(PATH_JOIN(window->props.selfDir, RESOURCE_PATH::MODELS, "primitives"), "plane50.obj");
+        meshes[mesh->GetMeshID()] = mesh;
+    }
+
+    // load tank obj
+    {
+        Mesh *mesh = new Mesh("tank_rails");
+        mesh->LoadMesh(PATH_JOIN(window->props.selfDir, RESOURCE_PATH::MODELS, "hw2tank"), "rails.obj");
+        meshes[mesh->GetMeshID()] = mesh;
+
+        mesh = new Mesh("tank_body");
+        mesh->LoadMesh(PATH_JOIN(window->props.selfDir, RESOURCE_PATH::MODELS, "hw2tank"), "body.obj");
+        meshes[mesh->GetMeshID()] = mesh;
+
+        mesh = new Mesh("tank_turret");
+        mesh->LoadMesh(PATH_JOIN(window->props.selfDir, RESOURCE_PATH::MODELS, "hw2tank"), "turret.obj");
+        meshes[mesh->GetMeshID()] = mesh;
+
+        mesh = new Mesh("tank_cannon");
+        mesh->LoadMesh(PATH_JOIN(window->props.selfDir, RESOURCE_PATH::MODELS, "hw2tank"), "cannon.obj");
         meshes[mesh->GetMeshID()] = mesh;
     }
 
     {
-        Mesh* mesh = new Mesh("sphere");
+        Mesh *mesh = new Mesh("sphere");
         mesh->LoadMesh(PATH_JOIN(window->props.selfDir, RESOURCE_PATH::MODELS, "primitives"), "sphere.obj");
         meshes[mesh->GetMeshID()] = mesh;
     }
 
     {
-        Mesh* mesh = new Mesh("plane");
-        mesh->LoadMesh(PATH_JOIN(window->props.selfDir, RESOURCE_PATH::MODELS, "primitives"), "plane50.obj");
-        meshes[mesh->GetMeshID()] = mesh;
-    }
-
-    // Create a shader program for drawing face polygon with the color of the normal
-    {
-        Shader *shader = new Shader("LabShader");
+        Shader *shader = new Shader("myShader");
         shader->AddShader(PATH_JOIN(window->props.selfDir, SOURCE_PATH::M1, "homework2", "shaders", "VertexShader.glsl"), GL_VERTEX_SHADER);
         shader->AddShader(PATH_JOIN(window->props.selfDir, SOURCE_PATH::M1, "homework2", "shaders", "FragmentShader.glsl"), GL_FRAGMENT_SHADER);
         shader->CreateAndLink();
         shaders[shader->GetName()] = shader;
     }
 
-    // Light & material properties
-    {
-        lightPosition = glm::vec3(0, 1, 1);
-        materialShininess = 50;
-        materialKd = 0.75;
-        materialKs = 0.75;
-    }
+    projectionMatrix = glm::perspective(RADIANS(fov), window->props.aspectRatio, .01f, 200.f);
 }
-
 
 void Homework2::FrameStart()
 {
@@ -74,88 +82,44 @@ void Homework2::FrameStart()
     glViewport(0, 0, resolution.x, resolution.y);
 }
 
-
 void Homework2::Update(float deltaTimeSeconds)
 {
     {
         glm::mat4 modelMatrix = glm::mat4(1);
-        modelMatrix = glm::translate(modelMatrix, glm::vec3(0, 1, 0));
-        RenderSimpleMesh(meshes["sphere"], shaders["LabShader"], modelMatrix, glm::vec3(0, 0, 1));
+        modelMatrix = glm::translate(modelMatrix, glm::vec3(0, 0, 0));
+        modelMatrix = glm::scale(modelMatrix, glm::vec3(3));
+        RenderMesh(meshes["plane"], shaders["myShader"], modelMatrix, glm::vec3(0.21f, 0.37f, 0.23f));
     }
 
     {
-        glm::mat4 modelMatrix = glm::mat4(1);
-        modelMatrix = glm::translate(modelMatrix, glm::vec3(2, 0.5f, 0));
-        modelMatrix = glm::rotate(modelMatrix, RADIANS(60.0f), glm::vec3(1, 0, 0));
-        modelMatrix = glm::scale(modelMatrix, glm::vec3(0.5f));
-        RenderSimpleMesh(meshes["box"], shaders["LabShader"], modelMatrix);
+        RenderTank();
     }
-
+    if (renderCameraTarget)
     {
         glm::mat4 modelMatrix = glm::mat4(1);
-        modelMatrix = glm::translate(modelMatrix, glm::vec3(-2, 0.5f, 0));
-        modelMatrix = glm::rotate(modelMatrix, RADIANS(60.0f), glm::vec3(1, 1, 0));
-        RenderSimpleMesh(meshes["box"], shaders["LabShader"], modelMatrix, glm::vec3(0, 0.5, 0));
-    }
-
-    // Render ground
-    {
-        glm::mat4 modelMatrix = glm::mat4(1);
-        modelMatrix = glm::translate(modelMatrix, glm::vec3(0, 0.01f, 0));
-        modelMatrix = glm::scale(modelMatrix, glm::vec3(0.25f));
-        RenderSimpleMesh(meshes["plane"], shaders["LabShader"], modelMatrix, glm::vec3(0.5f, 0.5f, 0.5f));
-    }
-
-    // Render the point light in the scene
-    {
-        glm::mat4 modelMatrix = glm::mat4(1);
-        modelMatrix = glm::translate(modelMatrix, lightPosition);
-        modelMatrix = glm::scale(modelMatrix, glm::vec3(0.1f));
-        RenderMesh(meshes["sphere"], shaders["Simple"], modelMatrix);
+        modelMatrix = glm::translate(modelMatrix, camera->GetTargetPosition());
+        modelMatrix = glm::scale(modelMatrix, glm::vec3(0.2f));
+        RenderMesh(meshes["sphere"], shaders["VertexNormal"], modelMatrix, glm::vec3(0, 0, 0));
     }
 }
-
 
 void Homework2::FrameEnd()
 {
-    DrawCoordinateSystem();
 }
 
-
-void Homework2::RenderSimpleMesh(Mesh *mesh, Shader *shader, const glm::mat4 & modelMatrix, const glm::vec3 &color)
+void Homework2::RenderMesh(Mesh *mesh, Shader *shader, const glm::mat4 &modelMatrix, const glm::vec3 &color)
 {
     if (!mesh || !shader || !shader->GetProgramID())
         return;
 
-    // Render an object using the specified shader and the specified position
     glUseProgram(shader->program);
 
-    // Set shader uniforms for light & material properties
-    // TODO(student): Set light position uniform
-    glUniform3f(glGetUniformLocation(shader->program, "light_position"), lightPosition.x, lightPosition.y, lightPosition.z);
-
-
-    glm::vec3 eyePosition = GetSceneCamera()->m_transform->GetWorldPosition();
-    // TODO(student): Set eye position (camera position) uniform
-    glUniform3f(glGetUniformLocation(shader->program, "eye_position"), eyePosition.x, eyePosition.y, eyePosition.z);
-
-    // TODO(student): Set material property uniforms (shininess, kd, ks, object color)
-    int materialShininess = 30;
-    float materialKd = 0.5f;
-    float materialKs = 0.5f;
-    glUniform1i(glGetUniformLocation(shader->program, "material_shininess"), materialShininess);
-    glUniform1f(glGetUniformLocation(shader->program, "material_kd"), materialKd);
-    glUniform1f(glGetUniformLocation(shader->program, "material_ks"), materialKs);
     glUniform3f(glGetUniformLocation(shader->program, "object_color"), color.x, color.y, color.z);
 
     // Bind model matrix
     GLint loc_model_matrix = glGetUniformLocation(shader->program, "Model");
     glUniformMatrix4fv(loc_model_matrix, 1, GL_FALSE, glm::value_ptr(modelMatrix));
-
-    // Bind view matrix
-    glm::mat4 viewMatrix = GetSceneCamera()->GetViewMatrix();
-    int loc_view_matrix = glGetUniformLocation(shader->program, "View");
-    glUniformMatrix4fv(loc_view_matrix, 1, GL_FALSE, glm::value_ptr(viewMatrix));
+    glUniformMatrix4fv(shader->loc_view_matrix, 1, GL_FALSE, glm::value_ptr(camera->GetViewMatrix()));
 
     // Bind projection matrix
     glm::mat4 projectionMatrix = GetSceneCamera()->GetProjectionMatrix();
@@ -167,70 +131,125 @@ void Homework2::RenderSimpleMesh(Mesh *mesh, Shader *shader, const glm::mat4 & m
     glDrawElements(mesh->GetDrawMode(), static_cast<int>(mesh->indices.size()), GL_UNSIGNED_INT, 0);
 }
 
-
-/*
- *  These are callback functions. To find more about callbacks and
- *  how they behave, see `input_controller.h`.
- */
-
-
 void Homework2::OnInputUpdate(float deltaTime, int mods)
 {
-    float speed = 2;
-
-    if (!window->MouseHold(GLFW_MOUSE_BUTTON_RIGHT))
+    if (window->MouseHold(GLFW_MOUSE_BUTTON_RIGHT))
     {
-        glm::vec3 up = glm::vec3(0, 1, 0);
-        glm::vec3 right = GetSceneCamera()->m_transform->GetLocalOXVector();
-        glm::vec3 forward = GetSceneCamera()->m_transform->GetLocalOZVector();
-        forward = glm::normalize(glm::vec3(forward.x, 0, forward.z));
 
-        // Control light position using on W, A, S, D, E, Q
-        if (window->KeyHold(GLFW_KEY_W)) lightPosition -= forward * deltaTime * speed;
-        if (window->KeyHold(GLFW_KEY_A)) lightPosition -= right * deltaTime * speed;
-        if (window->KeyHold(GLFW_KEY_S)) lightPosition += forward * deltaTime * speed;
-        if (window->KeyHold(GLFW_KEY_D)) lightPosition += right * deltaTime * speed;
-        if (window->KeyHold(GLFW_KEY_E)) lightPosition += up * deltaTime * speed;
-        if (window->KeyHold(GLFW_KEY_Q)) lightPosition -= up * deltaTime * speed;
+        if (window->KeyHold(GLFW_KEY_W))
+        {
+            // Translate the camera forward
+            camera->TranslateForward(deltaTime * cameraSpeed);
+        }
+        if (window->KeyHold(GLFW_KEY_A))
+        {
+            // Translate the camera to the left
+            camera->TranslateRight(-deltaTime * cameraSpeed);
+        }
+        if (window->KeyHold(GLFW_KEY_S))
+        {
+            // Translate the camera backwards
+            camera->TranslateForward(-deltaTime * cameraSpeed);
+        }
+        if (window->KeyHold(GLFW_KEY_D))
+        {
+            // Translate the camera to the right
+            camera->TranslateRight(deltaTime * cameraSpeed);
+        }
+        if (window->KeyHold(GLFW_KEY_Q))
+        {
+            // Translate the camera down
+            camera->TranslateUpward(-deltaTime * cameraSpeed);
+        }
+        if (window->KeyHold(GLFW_KEY_E))
+        {
+            // Translate the camera up
+            camera->TranslateUpward(deltaTime * cameraSpeed);
+        }
     }
 }
-
 
 void Homework2::OnKeyPress(int key, int mods)
 {
     // Add key press event
+    if (key == GLFW_KEY_T)
+    {
+        renderCameraTarget = !renderCameraTarget;
+    }
 }
-
 
 void Homework2::OnKeyRelease(int key, int mods)
 {
     // Add key release event
 }
 
-
 void Homework2::OnMouseMove(int mouseX, int mouseY, int deltaX, int deltaY)
 {
     // Add mouse move event
-}
 
+    if (window->MouseHold(GLFW_MOUSE_BUTTON_RIGHT))
+    {
+        float sensivityOX = 0.001f;
+        float sensivityOY = 0.001f;
+
+        if (window->GetSpecialKeyState() == 0)
+        {
+            camera->RotateThirdPerson_OX(sensivityOX * -deltaY);
+            camera->RotateThirdPerson_OY(sensivityOY * -deltaX);
+        }
+    }
+}
 
 void Homework2::OnMouseBtnPress(int mouseX, int mouseY, int button, int mods)
 {
     // Add mouse button press event
 }
 
-
 void Homework2::OnMouseBtnRelease(int mouseX, int mouseY, int button, int mods)
 {
     // Add mouse button release event
 }
 
-
 void Homework2::OnMouseScroll(int mouseX, int mouseY, int offsetX, int offsetY)
 {
 }
 
-
 void Homework2::OnWindowResize(int width, int height)
 {
+}
+
+void Homework2::RenderTank()
+{
+    float x_spawn_rotate = 270.f;
+    float z_spawn_rotate = 180.f;
+
+    glm::vec3 tank_position = glm::vec3(0, 0, 0);
+
+    glm::mat4 modelMatrix = glm::mat4(1);
+    modelMatrix = glm::translate(modelMatrix, tank_position);
+    modelMatrix = glm::scale(modelMatrix, glm::vec3(0.1f));
+    modelMatrix = glm::rotate(modelMatrix, RADIANS(x_spawn_rotate), glm::vec3(1, 0, 0));
+    modelMatrix = glm::rotate(modelMatrix, RADIANS(z_spawn_rotate), glm::vec3(0, 0, 1));
+    RenderMesh(meshes["tank_body"], shaders["myShader"], modelMatrix, glm::vec3(0.27f, 0.71f, 0.28f));
+
+    modelMatrix = glm::mat4(1);
+    modelMatrix = glm::translate(modelMatrix, tank_position);
+    modelMatrix = glm::scale(modelMatrix, glm::vec3(0.1f));
+    modelMatrix = glm::rotate(modelMatrix, RADIANS(x_spawn_rotate), glm::vec3(1, 0, 0));
+    modelMatrix = glm::rotate(modelMatrix, RADIANS(z_spawn_rotate), glm::vec3(0, 0, 1));
+    RenderMesh(meshes["tank_turret"], shaders["myShader"], modelMatrix, glm::vec3(0.72f, 0.67f, 0.74f));
+
+    modelMatrix = glm::mat4(1);
+    modelMatrix = glm::translate(modelMatrix, tank_position);
+    modelMatrix = glm::scale(modelMatrix, glm::vec3(0.1f));
+    modelMatrix = glm::rotate(modelMatrix, RADIANS(x_spawn_rotate), glm::vec3(1, 0, 0));
+    modelMatrix = glm::rotate(modelMatrix, RADIANS(z_spawn_rotate), glm::vec3(0, 0, 1));
+    RenderMesh(meshes["tank_cannon"], shaders["myShader"], modelMatrix, glm::vec3(0.07f, 0.41f, 0.21f));
+
+    modelMatrix = glm::mat4(1);
+    modelMatrix = glm::translate(modelMatrix, tank_position);
+    modelMatrix = glm::scale(modelMatrix, glm::vec3(0.1f));
+    modelMatrix = glm::rotate(modelMatrix, RADIANS(x_spawn_rotate), glm::vec3(1, 0, 0));
+    modelMatrix = glm::rotate(modelMatrix, RADIANS(z_spawn_rotate), glm::vec3(0, 0, 1));
+    RenderMesh(meshes["tank_rails"], shaders["myShader"], modelMatrix, glm::vec3(0.17f, 0.18f, 0.19f));
 }
--- src/lab_m1/homework2/shaders/FragmentShader.glsl
+++ src/lab_m1/homework2/shaders/FragmentShader.glsl
@@ -1,7 +1,6 @@
 #version 330
 
 // Input
-// TODO(student): Get color value from vertex shader
 in vec3 color;
 // Output
 layout(location = 0) out vec4 out_color;
@@ -9,7 +8,6 @@ layout(location = 0) out vec4 out_color;
 
 void main()
 {
-    // TODO(student): Write pixel out color
     out_color = vec4(color, 1.0);
 
 }
